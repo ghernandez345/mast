b3272c0496fa8ec37fb492c7096012768ea28be7
Gabe Hernandez <ghernandez345@gmail.com>
2013-08-26 11:22:21 -0500

    starting point

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,42 @@
 module.exports = function(grunt) {
 
-	'use strict';
+  'use strict';
 
-	// Order of the source files that we want to concat
-	var srcFiles = [
-		// Dependencies
-		'lib/deps/$.js',
-		'lib/deps/_.js',
-		'lib/deps/Backbone.js',
-		'lib/deps/async.js',
+  // Order of the source files that we want to concat
+  var srcFiles = [
+    'lib/Framework.js'
+  ];
 
-		// Core
-		'lib/Framework.js',
-		'lib/Logger.js',
-		'lib/Util.js',
-		'lib/touch.js',
-		'lib/define.js',
-		'lib/Component.js',
-		'lib/Region.js',
-		'lib/Data.js',
-		'lib/Router.js',
-		'lib/Comet.js',
-		'lib/ready.js',
-		'lib/raise.js'
-	];
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: srcFiles,
+        dest: 'dist/mast.js'
+      }
+    },
 
-		concat: {
-			dist: {
-				src: srcFiles,
-				dest: 'mast.dev.js'
-			}
-		},
-
-		uglify: {
-			options: {
-				report: 'min',
-				preserveComments: false
-			},
-			files: {
-				src: 'mast.dev.js',
-				dest: 'mast.min.js'
-			}
-		}
-	});
+    uglify: {
+      options: {
+        report: 'min',
+        preserveComments: false
+      },
+      files: {
+        src: 'dist/mast.js',
+        dest: 'dist/mast.min.js'
+      }
+    }
+  });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', [
-		'build:dev',
-		'build:production'
-	]);
+  grunt.registerTask('default', [
+    'build:dev',
+    'build:production'
+  ]);
 
-	grunt.registerTask('build:dev', ['concat']);
-	grunt.registerTask('build:production', ['concat', 'uglify']);
+  grunt.registerTask('build:dev', ['concat']);
+  grunt.registerTask('build:production', ['concat', 'uglify']);
 };
